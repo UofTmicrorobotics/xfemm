1f55c8cf168b4c8220c95bfe75d403742152c5f3
Stephen Yang <stepheny755@gmail.com>
2026-04-26 16:42:58 -0400

    [clang] update (l/r)trim with c++11 offerings from the same SO answer

--- libfemm/stringTools.h
+++ libfemm/stringTools.h
@@ -45,8 +45,9 @@ inline void to_lower(std::string &s)
  * @param s the string to be transformed in place
  */
 inline void ltrim(std::string &s) {
-    s.erase(s.begin(), std::find_if(s.begin(), s.end(),
-                                    std::not1(std::ptr_fun<int, int>(std::isspace))));
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
+        return !std::isspace(ch);
+    }));
 }
 
 /**
@@ -55,8 +56,9 @@ inline void ltrim(std::string &s) {
  * @param s the string to be transformed in place
  */
 inline void rtrim(std::string &s) {
-    s.erase(std::find_if(s.rbegin(), s.rend(),
-                         std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
+    s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
+        return !std::isspace(ch);
+    }).base(), s.end());
 }
 
 /**
